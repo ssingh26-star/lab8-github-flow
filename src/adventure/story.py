f9290c8f26bafadada397d7c2ee5e478a3d2a981
Shreya Singh <231575636+ssingh26-star@users.noreply.github.com>
2025-10-30 02:19:42 +0000

    texts are styled in different colors

--- src/adventure/story.py
+++ src/adventure/story.py
@@ -1,5 +1,6 @@
 from adventure.utils import read_events_from_file
 import random
+from rich import print
 
 def step(choice: str, events):
     random_event = random.choice(events)
@@ -9,18 +10,18 @@ def step(choice: str, events):
     elif choice == "right":
         return right_path(random_event)
     else:
-        return "You stand still, unsure what to do. The forest swallows you."
+        return "[orange4]You stand still, unsure what to do. The forest swallows you. [/orange4]"
 
 def left_path(event):
-    return "You walk left. " + event
+    return "[purple]You walk left. [/purple]" + event
 
 def right_path(event):
-    return "You walk right. " + event
+    return "[blue]You walk right. [/blue]" + event
 
 if __name__ == "__main__":
     events = read_events_from_file('events.txt')
 
-    print("You wake up in a dark forest. You can go left or right.")
+    print("[green]You wake up in a dark forest. You can go left or right.[/green]")
     while True:
         choice = input("Which direction do you choose? (left/right/exit): ")
         choice = choice.strip().lower()
